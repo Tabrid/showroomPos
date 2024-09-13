e324f12b284bd4a08463f9a69bcbe55ed2e7b96e
tabrid <mollik15-5096@diu.edu.bd>
2024-09-13 14:05:53 +0600

    best selling api

--- src/Components/services/baseUrl.jsx
+++ src/Components/services/baseUrl.jsx
@@ -1,4 +1,4 @@
 //  const baseUrl = 'https://api.v1.estarch.online'
- const baseUrl = 'http://localhost:5000'
-//  const baseUrl = 'https://api.showroom.estarch.com.bd'
+//  const baseUrl = 'http://localhost:5000'
+ const baseUrl = 'https://api.showroom.estarch.com.bd'
  export default baseUrl;
